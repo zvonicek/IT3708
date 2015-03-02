9767f657e61bc8510d44325e1edcd25be788d4d9
Petr Zvoníček <zvonicek@gmail.com>
2015-03-02 19:59:57 +0100

    task2: parent selection

--- task2_evolution/evolutionary/ea/selection.py
+++ task2_evolution/evolutionary/ea/selection.py
@@ -1,7 +1,9 @@
 import abc
-from functools import reduce
 import heapq
+from operator import methodcaller
 import random
+import math
+from numpy import std, mean
 
 
 # adult selectors
@@ -44,15 +46,43 @@ class AbstractParentSelector(metaclass=abc.ABCMeta):
 
 class FitnessProportionateParentSelector(AbstractParentSelector):
     def select_one(self, population: 'Population'):
-        max_val = sum(individual.fitness() for individual in population.individuals)
-        pick = random.uniform(0, max_val)
+        fitnesses = list(map(lambda x: x.fitness(), population.individuals))
+
+        exp_vals = self.exp_vals(fitnesses)
+        pick = random.uniform(0, len(fitnesses))
         current = 0
-        for individual in population.individuals:
-            current += individual.fitness()
+        for val in exp_vals:
+            current += val
             if current > pick:
-                return individual
+                return population.individuals[exp_vals.index(val)]
+
+    def exp_vals(self, fitnesses):
+        return list(map(lambda x: x/mean(fitnesses), fitnesses))
 
 
-class SigmaScalingParentSelector(AbstractParentSelector):
+class SigmaScalingParentSelector(FitnessProportionateParentSelector):
+    def exp_vals(self, fitnesses):
+        return list(map(lambda x: 1 + (x - mean(fitnesses))/(2*std(fitnesses)), fitnesses))
+
+
+class BoltzmannParentSelector(FitnessProportionateParentSelector):
+    def __init__(self, t):
+        self.t = t
+
+    def exp_vals(self, fitnesses):
+        x = [math.exp(x / self.t) for x in fitnesses]
+        return [nom/mean(x) for nom in x]
+
+
+class TournamentParentSelector(AbstractParentSelector):
+    def __init__(self, group_size, epsilon):
+        self.group_size = group_size
+        self.epsilon = epsilon
+
     def select_one(self, population: 'Population'):
-        pass
+        pick = random.sample(population.individuals, self.group_size)
+
+        if random.uniform(0, 1) > self.epsilon:
+            return max(pick, key=methodcaller('fitness'))
+        else:
+            return random.choice(pick)
