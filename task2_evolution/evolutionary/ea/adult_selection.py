033c426f0cc5cb211473362ba449b609efab5360
Petr Zvoníček <zvonicek@gmail.com>
2015-03-02 20:15:55 +0100

    task2: adult/parent selection refactoring

---
+++ task2_evolution/evolutionary/ea/adult_selection.py
@@ -0,0 +1,34 @@
+import abc
+import heapq
+
+
+class AbstractAdultSelector(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def select(self, population: 'Population'):
+        pass
+
+
+class FullGenerationalReplacementAdultSelector(AbstractAdultSelector):
+    def select(self, population: 'Population'):
+        population.individuals = population.generate(population.population_size)
+
+
+class OverProductionAdultSelector(AbstractAdultSelector):
+    def __init__(self, num_generated_children):
+        self.num_generated_children = num_generated_children
+
+    def select(self, population: 'Population'):
+        offspring = population.generate(self.num_generated_children)
+        population.individuals = heapq.nlargest(population.population_size, offspring, key=lambda x: x.fitness())
+
+
+class GenerationalMixingAdultSelector(AbstractAdultSelector):
+    def __init__(self, num_generated_children):
+        self.num_generated_children = num_generated_children
+
+    def select(self, population: 'Population'):
+        pool = population.individuals
+        pool += population.generate(self.num_generated_children)
+        population.individuals = heapq.nlargest(population.population_size, pool, key=lambda x: x.fitness())
+
+
--- task2_evolution/evolutionary/ea/parent_selection.py
+++ task2_evolution/evolutionary/ea/parent_selection.py
@@ -1,43 +1,10 @@
 import abc
-import heapq
 from operator import methodcaller
 import random
 import math
 from numpy import std, mean
 
 
-# adult selectors
-class AbstractAdultSelector(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def select(self, population: 'Population'):
-        pass
-
-
-class FullGenerationalReplacementAdultSelector(AbstractAdultSelector):
-    def select(self, population: 'Population'):
-        population.individuals = population.generate(population.population_size)
-
-
-class OverProductionAdultSelector(AbstractAdultSelector):
-    def __init__(self, num_generated_children):
-        self.num_generated_children = num_generated_children
-
-    def select(self, population: 'Population'):
-        offspring = population.generate(self.num_generated_children)
-        population.individuals = heapq.nlargest(population.population_size, offspring, key=lambda x: x.fitness())
-
-
-class GenerationalMixingAdultSelector(AbstractAdultSelector):
-    def __init__(self, num_generated_children):
-        self.num_generated_children = num_generated_children
-
-    def select(self, population: 'Population'):
-        pool = population.individuals
-        pool += population.generate(self.num_generated_children)
-        population.individuals = heapq.nlargest(population.population_size, pool, key=lambda x: x.fitness())
-
-
-# parent selectors
 class AbstractParentSelector(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def select_one(self, population: 'Population'):
