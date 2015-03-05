9d0ce2d85f33087f70e2b6f071546ad4ade21ffb
Petr Zvoníček <zvonicek@gmail.com>
2015-03-05 11:10:47 +0100

    task2: fitness unit tests

---
+++ task2_evolution/evolutionary/tests/test_LOLZFitnessEvaluator.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from lolz import LOLZFitnessEvaluator
+
+__author__ = 'zvonicek'
+
+
+class TestLOLZFitnessEvaluator(TestCase):
+    def test_get_fitness(self):
+        ev = LOLZFitnessEvaluator(4)
+
+        self.assertEqual(ev.get_fitness([0, 0, 0, 0, 0, 0]), 4/6)
+        self.assertEqual(ev.get_fitness([1, 1, 1, 1, 1, 1]), 1)
+        self.assertEqual(ev.get_fitness([1, 1, 0, 1, 0, 1]), 2/6)
+        self.assertEqual(ev.get_fitness([0, 0, 1, 0, 1, 1]), 2/6)
---
+++ task2_evolution/evolutionary/tests/test_oneMaxFitnessEvaluator.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from one_max_problem import OneMaxFitnessEvaluator
+
+__author__ = 'zvonicek'
+
+
+class TestOneMaxFitnessEvaluator(TestCase):
+    def test_get_fitness(self):
+        ev = OneMaxFitnessEvaluator()
+
+        self.assertEqual(ev.get_fitness([0, 0, 0]), 0)
+        self.assertEqual(ev.get_fitness([0, 1, 0, 1]), 0.5)
+        self.assertEqual(ev.get_fitness([1, 1, 1, 1]), 1.0)
