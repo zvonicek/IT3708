804e3561adf20a44cd5dab0aff7f6b9ffea53585
Petr Zvoníček <zvonicek@gmail.com>
2015-03-10 10:36:23 +0100

    task2: bootstrap loader

--- task2_evolution/evolutionary/main.py
+++ task2_evolution/evolutionary/main.py
@@ -1,11 +1,20 @@
+import sys
 from ea_impl.suprising_sequences import SurprisingEA
 import yaml
 import ea.config
 
 
-cfg = yaml.load(open('surprising.yaml', 'r'))
+config_file = 'one_max.yaml'
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        config_file = sys.argv[1]
+
+
+cfg = yaml.load(open(config_file, 'r'))
 for key, val in cfg.items():
     setattr(ea.config, key, val)
 
-ea = SurprisingEA()
+ea = ea.config.ea
+ea.__init__()
 ea.run()
