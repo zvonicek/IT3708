b55ef53f885e89ffd1c9d5423798083ef977f5c8
Petr Zvoníček <zvonicek@gmail.com>
2015-03-24 15:49:08 +0100

    flatland: fixed main

--- task3_neural/flatland/main.py
+++ task3_neural/flatland/main.py
@@ -1,9 +1,14 @@
 from tkinter import Tk
-from flatland.flatnand import Flatland
+from flatland.flatland import Flatland, Turn
 from gui.gui import GUI
 
-f = Flatland(10, (1/3, 1/3))
+f = Flatland(10, (1/3, 1/3), (2, 2))
+
 tk = Tk()
 gui = GUI(tk)
+
 gui.draw_flatland(f)
+
+gui.draw_flatland(f)
+
 tk.mainloop()
