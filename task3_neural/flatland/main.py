355a7fbab92d0ca4636369c8bc09a2f92eaf8221
Petr Zvoníček <zvonicek@gmail.com>
2015-04-01 15:41:00 +0200

    Flatland: command line parser

--- task3_neural/flatland/main.py
+++ task3_neural/flatland/main.py
@@ -1,6 +1,26 @@
+import getopt
+import sys
 from flatland.flatland_ann import FlatlandAnnFactory
 from flatland.flatland_ea import FlatlandEA
 
+
+dynamic = False
+scenarios = 1
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ds:", ["dynamic", "scenarios="])
+    except getopt.GetoptError as e:
+        print('main.py -d True -s 5')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-d':
+            dynamic = True
+        elif opt in ("-s", "--scenarios"):
+            scenarios = int(arg)
+
+print("computing dynamic:", dynamic, "with", scenarios, "scenarios")
+
 ann = FlatlandAnnFactory().create()
-ea = FlatlandEA(ann, True, 5)
+ea = FlatlandEA(ann, dynamic, scenarios)
 ea.run()
