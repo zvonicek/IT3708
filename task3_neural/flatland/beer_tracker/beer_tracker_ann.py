c16b18de1dc35930f3455d6d4366c4964bbffbb6
Tesarova <evatesarov@gmail.com>
2015-04-18 07:11:39 +0200

    BeerTrackerAnn: factory for neural network for beer tracker

---
+++ task3_neural/flatland/beer_tracker/beer_tracker_ann.py
@@ -0,0 +1,24 @@
+from ann.activation import SigmoidActivation
+from ann.ann import AbstractAnnFactory
+from ann.layer import Layer
+from ann.network import Network
+from ann.neuron import CtrnnNeuron
+
+
+class BeerTrackerAnnFactory(AbstractAnnFactory):
+    def create(self):
+        activation_func = SigmoidActivation()
+        hidden_layer = []
+        output_layer = []
+
+        for i in range(2):
+            # time constant: [1,2]
+            hidden_layer.append(CtrnnNeuron(5, activation_func, 1, 2))
+
+        for i in range(2):
+            # time constant: [1,2]
+            output_layer.append(CtrnnNeuron(2, activation_func, 1, 2))
+
+        network = Network([Layer(hidden_layer), Layer(output_layer)])
+
+        return network
