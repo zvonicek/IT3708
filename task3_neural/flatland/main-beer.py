d956b83bf7e354aac5f9fb38595d9c275e62d547
Tesarova <evatesarov@gmail.com>
2015-04-18 07:17:12 +0200

    Main: just added ann to run

--- task3_neural/flatland/main-beer.py
+++ task3_neural/flatland/main-beer.py
@@ -1,10 +1,11 @@
 from tkinter import Tk
 from beer_tracker.world import World
 from beer_tracker.gui import GUI
+from beer_tracker.beer_tracker_ann import BeerTrackerAnnFactory
 
 beer = World()
-
+ann = BeerTrackerAnnFactory().create()
 tk = Tk()
 gui = GUI(tk)
-gui.play(beer, None)
+gui.play(beer, ann)
 tk.mainloop()
