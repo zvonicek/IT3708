352cbc1d93654922a48cd33c1893ff6148d30259
Tesarova <evatesarov@gmail.com>
2015-04-18 07:08:30 +0200

    Network: added method for set weights for CTRNN

--- task3_neural/flatland/ann/network.py
+++ task3_neural/flatland/ann/network.py
@@ -18,3 +18,42 @@ class Network():
 
     def weights_count(self):
         return sum(x.weights_count() for x in self.layers)
+
+    def neurons_count(self):
+        return sum(map(lambda x: len(x), self.layers))
+
+
+class CtrnnNetwork(Network):
+
+    # Ty neurony je treba prochazet tak blbe nekolikrat, protoze chci zachovat poradi tech vah v poli (napr. bias ma jiny rozsah nez gain)
+    def set_weights(self, weights):
+        i = 0
+
+        for layer in self.layers:
+            for neuron in layer.neurons:
+                # weights among layers
+                for j in range(len(neuron.weights)):
+                    neuron.weights[j] = weights[i]
+                    i += 1
+                # weights inside layer
+                for j in range(neuron.layer_neuron_num):
+                    neuron.my_layer_weights[j] = weights[i]
+                    i += 1
+
+        for layer in self.layers:
+            for neuron in layer.neurons:
+                # set bias weight
+                neuron.bias_weight = weights[i]
+                i += 1
+
+        for layer in self.layers:
+            for neuron in layer.neurons:
+                # set gain
+                neuron.gain = weights[i]
+                i += 1
+
+        for layer in self.layers:
+            for neuron in layer.neurons:
+                # set time constant
+                neuron.time_constant = weights[i]
+                i += 1
