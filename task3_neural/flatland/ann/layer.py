873dfe32b462f5245526387abdc31cbe4e101bf3
Tesarova <evatesarov@gmail.com>
2015-04-18 07:10:17 +0200

    Layer: adjust function for computing number of weights in CTRNN ann

--- task3_neural/flatland/ann/layer.py
+++ task3_neural/flatland/ann/layer.py
@@ -11,3 +11,17 @@ class Layer():
 
     def weights_count(self):
         return sum(x.weights_count for x in self.neurons)
+
+
+class CtrnnLayer(Layer):
+    def compute(self, vals):
+        res = []
+        # weight for self loops, ...
+        layer_vals = list(x.output for x in self.neurons)
+        for neuron in self.neurons:
+            res.append(neuron.compute(vals, layer_vals))
+
+        return res
+
+    def weights_count(self):
+        return sum(x.weights_count + x.layer_neuron_num for x in self.neurons)
