f18345eec5d944e9107448841ceab09b9bd8e68d
Tesarova <evatesarov@gmail.com>
2015-04-20 11:01:23 +0200

    Activation: identity activation function added

--- task3_neural/flatland/ann/activation.py
+++ task3_neural/flatland/ann/activation.py
@@ -13,6 +13,11 @@ class StepActivation(AbstractActivation):
         return value > 0
 
 
+class SimpleActivation(AbstractActivation):
+    def get_output(self, value):
+        return value
+
+
 class SigmoidActivation(AbstractActivation):
     def get_output(self, value):
         return 1 / (1 + exp(-value))
