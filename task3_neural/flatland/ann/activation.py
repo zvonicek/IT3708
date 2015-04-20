c4f45558e11b7fa683c5ac0273cda219773aedef
Tesarova <evatesarov@gmail.com>
2015-04-20 11:46:42 +0200

    Activation: tangent activation added

--- task3_neural/flatland/ann/activation.py
+++ task3_neural/flatland/ann/activation.py
@@ -21,3 +21,8 @@ class SimpleActivation(AbstractActivation):
 class SigmoidActivation(AbstractActivation):
     def get_output(self, value):
         return 1 / (1 + exp(-value))
+
+
+class TangentActivation(AbstractActivation):
+    def get_output(self, value):
+        return (exp(2*value) - 1) / (exp(2*value) + 1)
