3863fda5c7690b008c0ab5d8496340876f1dc6e4
Petr Zvoníček <zvonicek@gmail.com>
2015-04-22 01:11:10 +0200

    Flatland: improved fitness
    I believe this should provide better results :-)

--- task3_neural/flatland/flatland/flatland.py
+++ task3_neural/flatland/flatland/flatland.py
@@ -115,7 +115,7 @@ class Flatland():
 
         reward = 0
         food_reward = 1
-        poison_punishment = 5
+        poison_punishment = 1
 
         # in assignment: 60 time step for moving in flatland
         for i in range(60):
