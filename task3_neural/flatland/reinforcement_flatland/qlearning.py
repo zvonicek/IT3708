7b28f9c6ca848a48e17cdb13afa1eca0b1f55ffb
Petr Zvoníček <zvonicek@gmail.com>
2015-05-05 15:33:14 +0200

    task5: eligibility, first try

--- task3_neural/flatland/reinforcement_flatland/qlearning.py
+++ task3_neural/flatland/reinforcement_flatland/qlearning.py
@@ -10,16 +10,20 @@ class QLearning():
             self.flatland = Flatland.from_file(world_filename)
 
         # probability of choosing random action
-        self.p = 0.2
-
+        self.p = 0.1
         # α
         self.learning_rate = 0.2
         # γ
         self.discount_rate = 0.9
+        # λ
+        self.trace_decay = 0.9
 
         self.food_remaining = None
         self.poison_remaining = None
+        # q values
         self.q = {}
+        # eligibility values
+        self.e = {}
         self.eaten = set()
 
     def q_learning(self):
@@ -27,8 +31,7 @@ class QLearning():
 
         for i in range(0, 99):
             self.reset()
-            #while self.food_remaining > 0 or self.flatland.agent_coord != self.flatland.agent_init: temp
-            while self.food_remaining > 0:
+            while self.food_remaining > 0 or self.flatland.agent_coord != self.flatland.agent_init:
                 prev_state = self.current_state()
 
                 # select action to do
@@ -64,7 +67,7 @@ class QLearning():
 
     def compute_reward(self, prev_artifact):
         if prev_artifact == Cell.Food:
-            return 1
+            return 5
         elif prev_artifact == Cell.Poison:
             return -5
         else:
@@ -84,14 +87,25 @@ class QLearning():
 
     def update_q(self, prev_state, new_state, action, reward):
         q_prev = self.q.get((prev_state, action), 0)
-        q_max_next = self.q.get(new_state, self.best_action(new_state))
+        q_best_next = self.q.get(new_state, self.best_action(new_state))
 
-        self.q[prev_state, action] = q_prev + self.learning_rate * (reward + self.discount_rate*q_max_next - q_prev)
+        self.q[prev_state, action] = q_prev + self.learning_rate * (reward + self.discount_rate*q_best_next - q_prev)
+
+        self.update_eligibility(prev_state, action, reward, q_best_next, q_prev)
+
+    def update_eligibility(self, prev_state, action, reward, q_best_next, q_prev):
+        delta = reward + self.discount_rate * q_best_next - q_prev
+        self.e[prev_state, action] = self.e.get((prev_state, action), 0) + 1
+
+        for key in self.q:
+            self.q[key] += self.learning_rate * delta * self.e.get(key, 0)
+            self.e[key] = self.discount_rate * self.trace_decay * self.e.get(key, 0)
 
     def simulate(self, move_callback):
         self.reset()
-        #while self.food_remaining > 0 or self.flatland.agent_coord != self.flatland.agent_init: temp
-        while self.food_remaining > 0:
+        if move_callback:
+            move_callback(self.flatland)
+        while self.food_remaining > 0 or self.flatland.agent_coord != self.flatland.agent_init:
             action = self.best_action(self.current_state())
             self.move(action)
             if move_callback:
