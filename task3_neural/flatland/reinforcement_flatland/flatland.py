5fb93bca7528713a3caa107c4156cbe3407fb73d
Petr Zvoníček <zvonicek@gmail.com>
2015-05-05 12:13:35 +0200

    task5: flatland changes

--- task3_neural/flatland/reinforcement_flatland/flatland.py
+++ task3_neural/flatland/reinforcement_flatland/flatland.py
@@ -5,17 +5,13 @@ class Cell:
     Food, Poison, Empty, Agent = range(0, 4)
 
 
-class Orientation:
-    Up, Right, Down, Left = range(0, 4)
-
-
 class Turn:
-    Left, Right, Straight = range(0, 3)
+    Left, Right, Up, Down = range(0, 4)
 
 
 class Flatland():
     def __init__(self, world, agent_coord, food_num, poison_num):
-        self.agent_orientation = Orientation.Up
+        self.agent_init = agent_coord
         self.agent_coord = agent_coord
         self.grid = world
         self.food_num = food_num
@@ -77,39 +73,20 @@ class Flatland():
     def reset(self):
         """resets the world to initial state"""
 
-        self.agent_orientation = Orientation.Up
         self.grid = [x[:] for x in self.start_grid]
-
-        # find initial agent position
-        for row in range(len(self.grid)):
-            for col in range(len(self.grid)):
-                if self.grid[row][col] == Cell.Agent:
-                    self.agent_coord = row, col
-                    break
-
-    def __turn(self, turn):
-        if turn == Turn.Left:
-            self.agent_orientation = (self.agent_orientation - 1) % 4
-        elif turn == Turn.Right:
-            self.agent_orientation = (self.agent_orientation + 1) % 4
-        elif turn == Turn.Straight:
-            pass
-        else:
-            raise Exception("Illegal turn")
+        self.agent_coord = self.agent_init
 
     def move(self, turn):
-        self.__turn(turn)
-
         curr_row, curr_col = self.get_agent()
         new_row, new_col = curr_row, curr_col
 
-        if self.agent_orientation == Orientation.Up:
+        if turn == Turn.Up:
             new_row = (new_row - 1) % len(self.grid)
-        elif self.agent_orientation == Orientation.Right:
+        elif turn == Turn.Right:
             new_col = (new_col + 1) % len(self.grid)
-        elif self.agent_orientation == Orientation.Down:
+        elif turn == Turn.Down:
             new_row = (new_row + 1) % len(self.grid)
-        elif self.agent_orientation == Orientation.Left:
+        elif turn == Turn.Left:
             new_col = (new_col - 1) % len(self.grid)
 
         cell_state = self.grid[new_row][new_col]
@@ -122,83 +99,6 @@ class Flatland():
 
     def get_agent(self):
         return self.agent_coord
-
-    def sensor_output(self):
-        x, y = self.get_agent()
-        l = len(self.grid)
-        result = []
-
-        if self.agent_orientation == Orientation.Up:
-            surround = [((x - 1) % l, y), (x, (y + 1) % l), ((x + 1) % l, y)]
-        elif self.agent_orientation == Orientation.Right:
-            surround = [(x, (y + 1) % l), ((x + 1) % l, y), (x, (y - 1) % l)]
-        elif self.agent_orientation == Orientation.Down:
-            surround = [((x + 1) % l, y), (x, (y - 1) % l), ((x - 1) % l, y)]
-        else:
-            surround = [(x, (y - 1) % l), ((x - 1) % l, y), (x, (y + 1) % l)]
-
-        for p in surround:
-            result.append(self.grid[p[0]][p[1]])
-
-        # result: [Left sensor, Straight sensor, Right sensor]
-        return result
-
-    def simulate(self, ann, move_callback=None, print_stats=False):
-        """
-        run simulation and return the fitness
-        :param ann ANN
-        :param move_callback optional callback called on each tick
-        :return fitness of the run
-        """
-
-        reward = 0
-        food_reward = 1
-        poison_punishment = 1
-
-        # TBD: while snezeny_poison == 0 and snezene_jidlo != len(jidlo) and position != start_position
-        for i in range(60):
-            sensor_output = self.sensor_output()
-            food_input = list(map(lambda x: 1 if x == Cell.Food else 0, sensor_output))
-            poison_input = list(map(lambda x: 1 if x == Cell.Poison else 0, sensor_output))
-            ann_input = food_input + poison_input
-            result = ann.compute(ann_input)
-            action = self.interpret_result(result)
-            eaten = self.move(action)
-
-            if eaten == Cell.Food:
-                reward += food_reward
-            elif eaten == Cell.Poison:
-                reward -= poison_punishment
-
-            # check if the callback was set
-            if move_callback:
-                move_callback(self)
-
-        # normalize reward to interval [0, 1]
-        min_value = self.poison_num * poison_punishment * -1
-        max_value = self.food_num * food_reward
-        reward = (reward - min_value) / (max_value - min_value)
-
-        if print_stats:
-            self.print_stats()
-
-        self.reset()
-
-        return reward
-
-    @staticmethod
-    def interpret_result(result):
-        directions = [Turn.Left, Turn.Straight, Turn.Right]
-
-        # choose the maximum output, if there are more, then randomly
-        max_o = max(result)
-        act = []
-
-        for d in directions:
-            if result[d] == max_o:
-                act.append(d)
-
-        return random.choice(act)
 
     def print_stats(self):
         food = 0
